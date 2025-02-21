a12df8c25050e522c545209ff5f1a621be555252
truong086 <91019609+truong086@users.noreply.github.com>
2025-02-22 02:38:50 +0800

    updatepushserve

--- quanlykhoupdate/Program.cs
+++ quanlykhoupdate/Program.cs
@@ -134,7 +134,7 @@ builder.Services.AddDistributedMemoryCache();
 builder.Services.AddHttpContextAccessor();
 builder.Services.AddHttpClient();
 
-//builder.WebHost.UseUrls("http://0.0.0.0:5000");
+builder.WebHost.UseUrls("http://0.0.0.0:5000");
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
