60e3decb59a9ffbc0e37817b179a111d279d9984
truong086 <91019609+truong086@users.noreply.github.com>
2025-02-25 20:11:47 +0800

    updatedata2

--- quanlykhoupdate/obj/Debug/net6.0/quanlykhoupdate.AssemblyInfo.cs
+++ quanlykhoupdate/obj/Debug/net6.0/quanlykhoupdate.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("quanlykhoupdate")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6543c1575469f8f06d394be7d10e38c435caadf1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("quanlykhoupdate")]
 [assembly: System.Reflection.AssemblyTitleAttribute("quanlykhoupdate")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
